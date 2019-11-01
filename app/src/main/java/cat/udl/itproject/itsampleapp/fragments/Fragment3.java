3c458f094b43ecfb2f618f3b78a8a514f38f6965
Sergi Trujillo <sergi.trujillo@gmail.com>
2019-11-01 16:20:59 +0100

    Activity to fragment comunication

--- app/src/main/java/cat/udl/itproject/itsampleapp/fragments/Fragment3.java
+++ app/src/main/java/cat/udl/itproject/itsampleapp/fragments/Fragment3.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,12 +14,19 @@ import cat.udl.itproject.itsampleapp.R;
 public class Fragment3 extends Fragment {
 
 
+    private TextView textView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment3, container, false);
+        View view = inflater.inflate(R.layout.fragment3, container, false);
+        textView = view.findViewById(R.id.textView);
+        return view;
     }
 
 
+    public void doSomething(String s) {
+        textView.setText(s);
+    }
 }
