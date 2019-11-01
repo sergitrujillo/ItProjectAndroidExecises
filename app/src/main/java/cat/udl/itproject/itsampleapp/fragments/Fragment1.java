38d4d7593fe0fa3a7cda417787a8cb89cb15c4e1
Sergi Trujillo <sergi.trujillo@gmail.com>
2019-11-01 15:50:46 +0100

    Create 2 static fragments

---
+++ app/src/main/java/cat/udl/itproject/itsampleapp/fragments/Fragment1.java
@@ -0,0 +1,23 @@
+package cat.udl.itproject.itsampleapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import cat.udl.itproject.itsampleapp.R;
+
+
+public class Fragment1 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+
+}
---
+++ app/src/main/java/cat/udl/itproject/itsampleapp/fragments/FragmentsActivity.java
@@ -0,0 +1,16 @@
+package cat.udl.itproject.itsampleapp.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cat.udl.itproject.itsampleapp.R;
+
+public class FragmentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragments);
+    }
+}
